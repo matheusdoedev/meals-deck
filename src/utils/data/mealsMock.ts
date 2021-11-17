8397a506524aef344e6bd9f7bad11820a7f9f8b7
matheusdoedev <matheusdoe.dev@gmail.com>
2021-11-16 21:04:37 -0300

    feat: mealsMock object

---
+++ src/utils/data/mealsMock.ts
@@ -0,0 +1,72 @@
+import { IMeal } from '~/models/Meal';
+
+export const mealsMock: IMeal[] = [
+  {
+    id: 1,
+    cover_image:
+      'https://images.unsplash.com/photo-1599599810694-b5b37304c041?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aG90JTIwZG9nc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    name: 'Hot Dogs',
+    ingredients: [
+      {
+        name: 'Bread',
+      },
+      {
+        name: 'Sauce',
+      },
+      {
+        name: 'Sausage',
+      },
+    ],
+  },
+  {
+    id: 2,
+    cover_image:
+      'https://images.unsplash.com/photo-1599599810694-b5b37304c041?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aG90JTIwZG9nc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    name: 'Hot Dogs',
+    ingredients: [
+      {
+        name: 'Bread',
+      },
+      {
+        name: 'Sauce',
+      },
+      {
+        name: 'Sausage',
+      },
+    ],
+  },
+  {
+    id: 3,
+    cover_image:
+      'https://images.unsplash.com/photo-1599599810694-b5b37304c041?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aG90JTIwZG9nc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    name: 'Hot Dogs',
+    ingredients: [
+      {
+        name: 'Bread',
+      },
+      {
+        name: 'Sauce',
+      },
+      {
+        name: 'Sausage',
+      },
+    ],
+  },
+  {
+    id: 4,
+    cover_image:
+      'https://images.unsplash.com/photo-1599599810694-b5b37304c041?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aG90JTIwZG9nc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    name: 'Hot Dogs',
+    ingredients: [
+      {
+        name: 'Bread',
+      },
+      {
+        name: 'Sauce',
+      },
+      {
+        name: 'Sausage',
+      },
+    ],
+  },
+];
